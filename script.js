c81e8241fe56ef3a18d81cf6a40627f1edb75be8
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2019-12-18 21:46:24 -0500

    added an event with a lower case array and uppercase array

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var lowerCaseArr = [
+      "a","b","c","d","e","f","g","h","i","j","k","l","m",
+      "n","o","p","q","r","s","t","u","v","w","x","y","z"
+    ];
+    
+    var upperCaseArr = [
+      "A","B","C","D","E","F","G","H","I","J","K","L","M",
+      "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+    ];
+
+    
